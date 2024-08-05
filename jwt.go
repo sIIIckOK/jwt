9502c06795ad0b6d639767ce77bc2f9df9015632
siiickok <siiickok@gmail.com>
2024-08-06 03:43:14 +0545

    Implement Verification of Secret Key

--- jwt.go
+++ jwt.go
@@ -1,25 +1,26 @@
 package jwt
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
+	"fmt"
 )
 
-// @TODO: Implement secret later
 type Token struct {
-    Header     Header
-    // secret     string
-    payloadStr string
-}
-
-func (t *Token) DecodePayload(v any) error {
-    return json.Unmarshal([]byte(t.payloadStr), v)
+    Header      Header
+    body        string
+    signature   string
+    payloadStr  string
 }
 
 type Header struct {
 	Type string `json:"typ"`
 	Alg  string `json:"alg"`
 }
+
 
 func DecodeToken(strToken string) (Token, error) {
     var t Token
@@ -38,21 +39,54 @@ func DecodeToken(strToken string) (Token, error) {
     t.Header = header
 
     var payloadLen uint32
-    _ = payloadLen
     for i, v := range strToken[headerLen+1:] {
         if v == '.' {
             payloadLen = uint32(i)
             break
         }
     }
+    t.body = strToken[:headerLen+payloadLen+1]
 
-    p, err := base64.RawURLEncoding.DecodeString(strToken[headerLen+1 : headerLen+1 + payloadLen])
+    pStr := strToken[headerLen+1 : headerLen+1 + payloadLen]
+    p, err := base64.RawURLEncoding.DecodeString(pStr)
     if err != nil { 
         return t, err 
     }
     t.payloadStr = string(p)
 
+    sig := strToken[headerLen + payloadLen + 2:]
+    t.signature = string(sig)
     return t, nil
+}
+
+func (t Token) VerifySecret(secret []byte) (bool, error) {
+    //@TODO: Implement RS256
+    if t.Header.Alg != "HS256" {
+        err := fmt.Sprintf("Hashing of `%v` is not supported", t.Header.Alg)
+        return false, errors.New(err)
+    }
+
+    enc := hs256ThenBase64([]byte(t.body), secret)
+
+    if enc == t.signature {
+        return true, nil
+    }
+
+    return false, nil
+}
+
+func hs256ThenBase64(data []byte, secret []byte) string {
+    h := hmac.New(sha256.New, secret)
+    h.Write(data)
+    hashed := h.Sum(nil)
+
+    s := base64.RawURLEncoding.EncodeToString(hashed)
+
+    return s
+}
+
+func (t Token) DecodePayload(v any) error {
+    return json.Unmarshal([]byte(t.payloadStr), v)
 }
 
 func decodeHeader(h string) (Header, error) {
